47d9472a9d7351e3e6b58e3affaf6f19c42644a8
Akash Kumar Behera <akashkumar48874@gmail.com>
2026-09-09 17:37:13 +0530

    Fix EdgeInsets syntax in HomeScreen to pass CI test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -162,7 +162,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 // In-App Update Banner if update is available
                 if (_latestRelease != null) ...[
                   Container(
-                    margin: const EdgeInsets.bottom: 24,
+                    margin: const EdgeInsets.only(bottom: 24),
                     padding: const EdgeInsets.all(16),
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
